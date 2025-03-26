a8c66333fac440d809fcd73fd112a5fa83988da0
Nguyễn Khánh Minh <nguyenkhanhminh251@gmail.com>
2025-03-26 23:35:09 +0700

    Add LinkedStack

---
+++ src/LinkedStack/LinkedStack.java
@@ -0,0 +1,31 @@
+package LinkedStack;
+
+import java.util.EmptyStackException;
+
+public class LinkedStack {
+    protected Node head;
+
+    public LinkedStack() { 
+        head = null;
+    }
+
+    public boolean isEmpty() { 
+        return(head==null);
+    }
+
+    public void push(Object x) { 
+        head = new Node(x,head);
+    }
+    Object top() throws EmptyStackException { 
+        if(isEmpty()) 
+            throw new EmptyStackException();
+        return(head.info);
+    }
+
+    public Object pop() throws EmptyStackException { 
+        if(isEmpty()) throw new EmptyStackException();
+        Object x = head.info;
+        head=head.next;
+        return(x);
+    }
+}
